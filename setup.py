661c49a1335c3c8ee409cc7297c8db440775198a
steve <stevegreghatch@gmail.com>
2024-05-20 14:42:40 -0400

    repo init

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 from setuptools import setup, find_packages
-from os import getenv
+from os import getenv, path
 
 setup(
     name='space-exploration-common-lib',
-    version=getenv('VERSION', default='v0.0.4'),
+    version=getenv('VERSION', default='v0.0.1'),
     packages=find_packages(exclude=['tests']),
     package_dir={'space_exploration_common_lib': 'src/space_exploration_common_lib'},
-    py_modules=[
-        'space_exploration_common_lib.model',
-        'space_exploration_common_lib.model.Duration',
-        'space_exploration_common_lib.model.mission.ProjectMercury'
-    ],
     python_requires='>=3.12, <4',
     include_package_data=True,
     install_requires=[
         'pydantic==2.7.1'
     ],
     author='Steven Hatch',
-    author_email='stevegreghatch@gmail.com'
+    author_email='stevegreghatch@gmail.com',
+    description='A common library for space exploration projects'
 )
