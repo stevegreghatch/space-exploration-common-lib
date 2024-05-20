a7ee4f7d66c37bf6365ca52ff21ed5e1c08a95b9
steve <stevegreghatch@gmail.com>
2024-05-20 14:35:09 -0400

    repo init

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ from os import getenv
 
 setup(
     name='space-exploration-common-lib',
-    version=getenv('VERSION', default='v0.0.1'),
-    packages=find_packages(where='src', exclude=['tests']),
+    version=getenv('VERSION', default='v0.0.4'),
+    packages=find_packages(exclude=['tests']),
+    package_dir={'space_exploration_common_lib': 'src/space_exploration_common_lib'},
+    py_modules=[
+        'space_exploration_common_lib.model',
+        'space_exploration_common_lib.model.Duration',
+        'space_exploration_common_lib.model.mission.ProjectMercury'
+    ],
     python_requires='>=3.12, <4',
     include_package_data=True,
     install_requires=[
         'pydantic==2.7.1'
     ],
-    scripts=["app.py"],
     author='Steven Hatch',
     author_email='stevegreghatch@gmail.com'
 )
