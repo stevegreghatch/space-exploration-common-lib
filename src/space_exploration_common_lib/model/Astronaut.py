1c365deec74907848b92ddfea8fa27b354a0c0ce
steve <stevegreghatch@gmail.com>
2024-05-30 16:26:30 -0400

    revised classes

--- src/space_exploration_common_lib/model/Astronaut.py
+++ src/space_exploration_common_lib/model/Astronaut.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel
 from typing import List
-from .Program import Program
 
 
 class Astronaut(BaseModel):
     astronaut_first_name: str
     astronaut_last_name: str
-    programs: List[Program]
     image_url: str
+    missions: List[str]
